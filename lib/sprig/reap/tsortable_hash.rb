2336965c840318ecda320d3a34a0111260ee3965
h0tl33t <h0tl33t@gmail.com>
2015-02-27 15:07:46 -0500

    Make TsortableHash able to resolve circular dependencies resulting from 2-way HABTM associations

--- lib/sprig/reap/tsortable_hash.rb
+++ lib/sprig/reap/tsortable_hash.rb
@@ -7,5 +7,20 @@ module Sprig::Reap
     def tsort_each_child(node, &block)
       fetch(node).each(&block)
     end
+
+    def resolve_circular_hatbm_dependencies!
+      # When two models each have a `has_and_belongs_to_many` association pointing to the other,
+      # it creates a circular dependency.  Based on Sprig documentation, we only need to define
+      # the association in one direction
+      # (https://github.com/vigetlabs/sprig#has-and-belongs-to-many), so we delete one of them.
+
+      self.each do |(model, dependencies)|
+        model.reflect_on_all_associations(:has_and_belongs_to_many).map do |association|
+          if dependencies.include? association.klass
+            self[association.klass] = self[association.klass] - [model]
+          end
+        end
+      end
+    end
   end
 end
---
+++ spec/lib/sprig/reap/tsortable_hash_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Sprig::Reap::TsortableHash do
+  describe "#resolve_circular_hatbm_dependencies!" do
+    subject do
+      described_class.new.merge(
+        Comment => [Post],
+        Post    => [User, Tag],
+        Tag     => [Post],
+        User    => [],
+        Vote    => [Post]
+      )
+    end
+
+    it "trims out circular dependencies resulting from has-and-belongs-to-many" do
+      subject.resolve_circular_hatbm_dependencies!
+      
+      subject.should == {
+        Comment => [Post],
+        Post    => [User, Tag],
+        Tag     => [],
+        User    => [],
+        Vote    => [Post]
+      }
+    end
+  end
+end
