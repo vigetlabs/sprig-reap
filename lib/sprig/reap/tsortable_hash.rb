8910d5b4876a74a365581b733e50e1f113ce749c
h0tl33t <h0tl33t@gmail.com>
2015-03-30 09:32:24 -0400

    Only resolve circular dependencies if both ends of the HABTM are in the model list (fix #22)

--- lib/sprig/reap/tsortable_hash.rb
+++ lib/sprig/reap/tsortable_hash.rb
@@ -16,7 +16,7 @@ module Sprig::Reap
 
       self.each do |(model, dependencies)|
         model.reflect_on_all_associations(:has_and_belongs_to_many).each do |association|
-          if dependencies.include? association.klass
+          if dependencies.include?(association.klass) && self[association.klass].present?
             self[association.klass] = self[association.klass] - [model]
           end
         end
